9647161ff915d6039fb283f5fd3de9d37005e3a2
Ramazan <raaaaaamazan@gmail.com>
2022-04-03 15:03:08 +0300

    ejs added

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 const express = require('express');
+const path =  require("path")
+const ejs = require("ejs")
+
 const app = new express();
 
-const port = 3000;
+//Template Engine
+app.set("view engine", "ejs")
+
+//Middleware
+app.use(express.static("public"))
 
 app.get('/', (req, res ) => {
-  const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
-  res.send(blog)
+  res.render("index")
 });
 
+app.get('/about', (req, res ) => {
+  res.render("about")
+});
+
+app.get('/add', (req, res ) => {
+  res.render("add_post")
+});
+
+
+const port = 3000;
 app.listen(port, () => {
   console.log(`Server started on ${port}`);
 });
